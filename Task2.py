8e38764c304e5d3a2587c7306f5561019aac0b81
Olesia Barannik <112662408+OlesiaBarannik@users.noreply.github.com>
2022-09-21 23:02:24 +0300

    Update Task2.py

--- Task2.py
+++ Task2.py
@@ -1,7 +1,6 @@
 def make_country(name, capital):
     country = {}
-    country['country'] = name
-    country['capital'] = capital
+    country[name] = capital
     return country
 
 my_country = make_country("Ukraine", "Kyiv")
